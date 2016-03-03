24a9e047ecbc6b9404f8f7da3f8e7258cff67cd2
Ashutosh Kumar Singh <ashu2016@gmail.com>
2016-03-03 12:56:30 +0530

    Video 2 done

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const Hapi = require('hapi')
+
+const server = new Hapi.Server()
+server.connection({
+    host:'localhost',
+    port:8000
+})
+
+let goodOptions = {
+    reporters:[{
+        reporter:require('good-console'),
+        events:{log:'*',response:'*'}
+    }]
+}
+
+server.register({
+    register:require('good'),
+    options:goodOptions
+},err =>{
+    server.route({
+    method:'GET',
+    path:'/',
+    handler:(request,reply) => {
+        server.log('error','Oh No !!!!')
+        server.log('info','Replying with proper data now!')
+        reply("hello hapi")
+    }
+})
+
+server.route({
+    method:'GET',
+    path:'/{name}',
+    handler:(request,reply) => {
+        reply('hello',request.params.name)
+    }
+})
+
+server.start(()=>console.log('started at:', server.info.uri))
+})
+ 
